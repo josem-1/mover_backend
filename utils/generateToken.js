cf594d63695c11963990e67edc9d2209d23950f0
josem-1 <161044437+josem-1@users.noreply.github.com>
2025-05-06 14:33:00 -0500

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -9,8 +9,8 @@ export const generateTokenAndSetCookie = (userId, res) => {
 		maxAge: 365 * 24 * 60 * 60 * 1000,
 		//not sure why but max age has to be in millliseconds accroding to that tim guy on yt so following the format days x hours x minutes x seconds x 1000(bcuz of stupid milliseconds
 		httpOnly: true, 
-		sameSite: "strict", 
-		secure: ENV_VARS.NODE_ENV !== "development", //still don't fully understand dev and prod difference in running, npm run dev vs run build???? the world will never know.....
+		sameSite: "none", 
+		secure: true, //still don't fully understand dev and prod difference in running, npm run dev vs run build???? the world will never know.....
 	});
 
 	return token;

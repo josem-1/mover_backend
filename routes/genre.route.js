d5e8b0de9d37a5d36ae4d2ffd62a0a59b024dc75
josem-1 <161044437+josem-1@users.noreply.github.com>
2025-06-11 14:16:49 -0500

    Update genre.route.js

--- routes/genre.route.js
+++ routes/genre.route.js
@@ -2,6 +2,5 @@ import express from 'express';
 import {getGenreMovies } from '../controllers/genre.controller.js';
 
 const router = express.Router();
-router.get('/:id', getGenreMovies);//for some bull crap reason, the whole program will crash out if this line is not correct. program is just like me fr
-
+router.get('/:id', getGenreMovies);
 export default router;
